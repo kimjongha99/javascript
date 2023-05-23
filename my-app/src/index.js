b19e6ed61906d20884af3b2f11a8c38dac3981da
kimjongha99 <jjhsun99@gmail.com>
2023-05-23 12:44:46 +0900

    05.23

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from './Library';
-import Clock  from './Clock';
+// import Library from './Library';
+// import Clock  from './Clock';
+// import Comment from './Comment';
+// import CommentList  from './CommentList';
+
+import NotificationList from './NotificationList';
+
+// import Notification from './Notification';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Clock />
-
+  <NotificationList />
   </React.StrictMode>
 );
 
